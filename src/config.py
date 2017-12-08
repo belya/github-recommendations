f72dcb4b7e7848ea322f158c433846e3ed0a3398
Katherine Surta <kate.surta@gmail.com>
2017-12-08 16:01:29 +0300

    move data directory walk and constants out of the github util module

---
+++ src/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+GITHUB_ACCESS_TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN')
+EVENTS = [
+    'CommitCommentEvent',
+    'CreateEvent',
+    'WatchEvent',
+    'ForkEvent',
+    'IssueEvent',
+    'IssueCommentEvent',
+    'PullRequestEvent',
+    'PushEvent',
+    'MemberEvent',
+]
--- src/utils/github.py
+++ src/utils/github.py
@@ -5,26 +5,10 @@ import os
 import re
 import requests
 
-from dotenv import load_dotenv, find_dotenv
 from bs4 import BeautifulSoup
 from markdown import markdown
 
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
-load_dotenv(dotenv_path)
-
-GITHUB_ACCESS_TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN')
-EVENTS = [
-    'CommitCommentEvent',
-    'CreateEvent',
-    'WatchEvent',
-    'ForkEvent',
-    'IssueEvent',
-    'IssueCommentEvent',
-    'PullRequestEvent',
-    'PushEvent',
-    'MemberEvent',
-]
+from config import GITHUB_ACCESS_TOKEN, EVENTS
 
 
 def retrieve_repo_readme(repo):
@@ -49,7 +33,7 @@ def retrieve_repo_languages(repo):
     return '/'.join(langs.keys())
 
 
-def collect_repos_description(data_files_pattern):
+def collect_event_data(fname):
     field_names = [
         'id',
         'type',
@@ -61,38 +45,37 @@ def collect_repos_description(data_files_pattern):
         'repo_languages',
     ]
 
-    for fname in glob.glob(data_files_pattern):
-        name, ext = os.path.splitext(fname)
+    name, ext = os.path.splitext(fname)
 
-        input_file = open(fname, 'r')
-        output_file = open(name + '.csv', 'w')
-        readme_file = open(name + '-readme', 'w')
+    input_file = open(fname, 'r')
+    output_file = open(name + '.csv', 'w')
+    readme_file = open(name + '-readme', 'w')
 
-        writer = csv.DictWriter(output_file, fieldnames=field_names)
-        writer.writeheader()
+    writer = csv.DictWriter(output_file, fieldnames=field_names)
+    writer.writeheader()
 
-        for line in input_file:
-            activity_record = json.loads(line)
-            # skip unnecessary event types that are not related to repos
-            if activity_record['type'] not in EVENTS:
-                continue
+    for line in input_file:
+        activity_record = json.loads(line)
+        # skip unnecessary event types that are not related to repos
+        if activity_record['type'] not in EVENTS:
+            continue
 
-            readme = clean_readme(
-                retrieve_repo_readme(activity_record['repo']))
-            langs = retrieve_repo_languages(activity_record['repo'])
-            data = {
-                'id': activity_record['id'],
-                'type': activity_record['type'],
-                'actor_id': activity_record['actor']['id'],
-                'actor_login': activity_record['actor']['login'],
-                'repo_id': activity_record['repo']['id'],
-                'repo_name': activity_record['repo']['name'],
-                'created_at': activity_record['created_at'],
-                'repo_languages': langs,
-            }
-            writer.writerow(data)
-            readme_file.write(readme + '\n\n')
+        readme = clean_readme(
+            retrieve_repo_readme(activity_record['repo']))
+        langs = retrieve_repo_languages(activity_record['repo'])
+        data = {
+            'id': activity_record['id'],
+            'type': activity_record['type'],
+            'actor_id': activity_record['actor']['id'],
+            'actor_login': activity_record['actor']['login'],
+            'repo_id': activity_record['repo']['id'],
+            'repo_name': activity_record['repo']['name'],
+            'created_at': activity_record['created_at'],
+            'repo_languages': langs,
+        }
+        writer.writerow(data)
+        readme_file.write(readme + '\n\n')
 
-        input_file.close()
-        output_file.close()
-        readme_file.close()
+    input_file.close()
+    output_file.close()
+    readme_file.close()
