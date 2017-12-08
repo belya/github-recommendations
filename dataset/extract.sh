967b96b661266ad56bdddd163b02887171228209
Blackmoon <belchikov1996@gmail.com>
2017-12-08 12:07:58 +0300

    Added new extraction script

--- dataset/extract.sh
+++ dataset/extract.sh
@@ -1,2 +1,4 @@
-wget http://data.githubarchive.org/2017-11-{01..30}-{0..23}.json.gz
-gunzip *.gz
+for DAY in `seq -w 30`
+do
+  wget http://data.githubarchive.org/2017-11-$DAY-{0..23}.json.gz &
+done
